2b82dc56f6e266fc7f73b71386ad6ff3bce3b0da
Haru <ryeo126@hotmail.com>
2025-04-23 13:53:01 +0800

    測試新增檔案

--- src/App.java
+++ src/App.java
@@ -1,7 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World! I am Edward No. 123456!");
+        System.out.println("Hello, World! 你好");
 
-        System.out.println("Hello, World! I am Edward No. 17!");
     }
 }
---
+++ src/App2.java
@@ -0,0 +1,8 @@
+public class App2 {
+    public class App {
+        public static void main(String[] args) throws Exception {
+            System.out.println("哈囉~你好");
+    
+        }
+    }
+}
